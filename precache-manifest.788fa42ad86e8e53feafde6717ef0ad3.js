4d5f28d06110c8ae8d4fe45c5a0f4828afdd076b
marialokodi <mrialokodi@gmail.com>
2022-07-01 00:19:54 +0300

    Updates

---
+++ precache-manifest.788fa42ad86e8e53feafde6717ef0ad3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a06000a2bd0940a7b684",
+    "url": "/eCommerce-website/static/css/main.81940f60.chunk.css"
+  },
+  {
+    "revision": "a06000a2bd0940a7b684",
+    "url": "/eCommerce-website/static/js/main.a06000a2.chunk.js"
+  },
+  {
+    "revision": "94421d0c55535ff39cc4",
+    "url": "/eCommerce-website/static/js/1.94421d0c.chunk.js"
+  },
+  {
+    "revision": "0f61e887b98a86b21088",
+    "url": "/eCommerce-website/static/css/2.26dd37ee.chunk.css"
+  },
+  {
+    "revision": "0f61e887b98a86b21088",
+    "url": "/eCommerce-website/static/js/2.0f61e887.chunk.js"
+  },
+  {
+    "revision": "9013b0a6b0c5c2a7aab2",
+    "url": "/eCommerce-website/static/js/runtime~main.9013b0a6.js"
+  },
+  {
+    "revision": "6da59dbf94579c32457bedb52c94256b",
+    "url": "/eCommerce-website/index.html"
+  }
+];
