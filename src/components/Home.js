80b9162b27ce0f85e7f597e406c239e38e574f55
marialokodi <mrialokodi@gmail.com>
2022-07-01 09:13:11 +0300

    correct

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,34 +1,26 @@
 import React from "react";
 import { Carousel, Button } from "react-bootstrap";
 import ProductCategories from "./ProductCategories";
+import { background } from "public/img/background.jpg";
+import { background2 } from "public/img/background2.jpg";
+import { background3 } from "public/img/background3.jpg";
+
 const Home = () => {
   return (
     <div>
       <Carousel>
         <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="public/img/background3.jpg"
-            alt="First slide"
-          />
+          <img className="d-block w-100" src={background} alt="First slide" />
           <Carousel.Caption>
             <Button variant="outline-light">Primary</Button>{" "}
             <Button variant="primary">Primary</Button>{" "}
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="public/img/background2.jpg"
-            alt="Second slide"
-          />
+          <img className="d-block w-100" src={background2} alt="Second slide" />
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="public/img/background3.jpg"
-            alt="Third slide"
-          />
+          <img className="d-block w-100" src={background3} alt="Third slide" />
         </Carousel.Item>
       </Carousel>
       <ProductCategories></ProductCategories>
