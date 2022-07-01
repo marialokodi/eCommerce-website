811ce730b6a6dcc4e35f4d15ae64cbd102d85cfb
marialokodi <mrialokodi@gmail.com>
2022-07-01 09:33:18 +0300

    img correction

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,35 @@
 import React from "react";
 import { Carousel, Button } from "react-bootstrap";
 import ProductCategories from "./ProductCategories";
-import { background } from "public/img/background.jpg";
-import { background2 } from "public/img/background2.jpg";
-import { background3 } from "public/img/background3.jpg";
 
 const Home = () => {
   return (
     <div>
       <Carousel>
         <Carousel.Item>
-          <img className="d-block w-100" src={background} alt="First slide" />
+          <img
+            className="d-block w-100"
+            src="https://firebasestorage.googleapis.com/v0/b/e-commerce-5e3a0.appspot.com/o/background3.jpg?alt=media&token=2a4e46c3-46a8-477e-9439-a2ec7307c781"
+            alt="First slide"
+          />
           <Carousel.Caption>
             <Button variant="outline-light">Primary</Button>{" "}
             <Button variant="primary">Primary</Button>{" "}
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img className="d-block w-100" src={background2} alt="Second slide" />
+          <img
+            className="d-block w-100"
+            src="https://firebasestorage.googleapis.com/v0/b/e-commerce-5e3a0.appspot.com/o/background2.jpg?alt=media&token=8d3fe3dd-68d9-411a-b8f9-4af740935f31"
+            alt="Second slide"
+          />
         </Carousel.Item>
         <Carousel.Item>
-          <img className="d-block w-100" src={background3} alt="Third slide" />
+          <img
+            className="d-block w-100"
+            src="https://firebasestorage.googleapis.com/v0/b/e-commerce-5e3a0.appspot.com/o/background3.jpg?alt=media&token=2a4e46c3-46a8-477e-9439-a2ec7307c781"
+            alt="Third slide"
+          />
         </Carousel.Item>
       </Carousel>
       <ProductCategories></ProductCategories>
